51f05dd05d29a2bb674bd118e1fcf3eb1ee581db
Sahyra <sahyra.palacios@tecnologicoliceocristiano.edu.ec>
2024-02-27 21:42:03 -0500

    Agregar archivos de proyecto.

---
+++ SEFAC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SEFAC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SEFAC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SEFAC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
